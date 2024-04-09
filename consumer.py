3c0de327d4e21b3ebbb285eb7c8506f4f8e4377d
dev-hack95 <saiprasadtoshatwad@gmail.com>
2024-04-09 21:39:49 +0530

    new changes

--- consumer.py
+++ consumer.py
@@ -4,11 +4,6 @@ import json
 import warnings
 from dotenv import load_dotenv
 from confluent_kafka import Consumer, Producer
-from sqlalchemy import create_engine
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import text
 from logger import logging
 
 sys.path.append("./audio")
@@ -18,63 +13,70 @@ from audio import convert
 # Config
 load_dotenv("./env")
 warnings.filterwarnings("ignore")
-Base = declarative_base()
+
+class KafkaProducer:
+    def __init__(self, bootstrap_servers: str, topic: str) -> None:
+        self.bootstrap_servers = bootstrap_servers
+        self.topic = topic
+        self.producer = Producer({
+            "bootstrap.servers": self.bootstrap_servers,
+        })
+
+    def produce_messages(self, message):
+        self.producer.produce(self.topic, value = json.dumps(message).encode('utf-8'))
+        
 
 
 
-# KafkaConfig
-topic1 = "Kafkatopic1"
-topic2 = "Kafkatopic2"
-
-producer = Producer(
-    {
-        "bootstrap.servers": "192.168.29.7:9092"
-    }
-)
-
-consumer = Consumer(
-    {
-        "bootstrap.servers": "192.168.29.7:9092",
-        "group.id": "group1",
+class KafkaConsumer:
+    def __init__(self, bootstrap_servers: str, group_id: str, topic: str) -> None:
+        self.consume = True
+        self.bootstrap_servers = bootstrap_servers
+        self.group_id = group_id
+        self.topic = topic
+        self.consumer = Consumer({
+        "bootstrap.servers": self.bootstrap_servers,
+        "group.id": self.group_id,
         "auto.offset.reset": "earliest",
-    }
-)
+        })
+ 
+        self.consumer.subscribe([self.topic])
 
-consumer.subscribe([topic1])
+    def consume_messages(self):
+        while self.consume:
+            message = self.consumer.poll(1.0)
 
+            if message is None:
+                continue
 
-consume = True
+            if message.error():
+               print("Consumer error: {}".format(message.error()))
+               continue
 
-while True:
-    if consume:
-        msg = consumer.poll(1.0)
-
-        if msg is None:
-            continue
-        if msg.error():
-            print("Consumer error: {}".format(msg.error()))
-            continue
-
-        data = json.loads(msg.value())
-        logging.info(data)
-        video = data.get("video")
-        email = data.get("email")
-
-        consume = False
-
-        t_instance = convert.Transform()
-        t_instance.create_folder()
-        t_instance.get_object(video)
-        mp3_file = t_instance.convert(video)
-        text = t_instance.transform(video)
-        print(text)
-        message = {
+            data = json.loads(message.value())
+            logging.info(data)
+            video = data.get("video")
+            email = data.get("email")
+    
+            self.consume = False
+    
+            t_instance = convert.Transform()
+            t_instance.create_folder()
+            t_instance.get_object(video)
+            mp3_file = t_instance.convert(video)
+            text = t_instance.transform(video)
+            message = {
             "email": email,
             "video": video,
             "audio": mp3_file,
             "text": str(text)
-        }
-        producer.produce(topic2, value = json.dumps(message).encode('utf-8'))
-        consume = True
+            }
+            producer.produce_messages(message)
+            self.consume = True
 
-    consumer.close()
+
+if __name__ == "__main__":
+    producer = KafkaProducer("192.168.29.7:9092", "Kafkatopic2")
+    consumer = KafkaConsumer("192.168.29.7:9092", "group1", "Kafkatopic1")
+    consumer.consume_messages()
+
